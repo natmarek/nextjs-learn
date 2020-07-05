a5b90d1732cb8bb23355ced7d1723ca8cf029937
ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-07-05 19:56:51 +0100

    cganged index.js

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Welcome to Next.js!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          I'm learning <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
