2a0bdc1075c4947fe5d4d709f62710b7202d57a4
ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-07-13 21:26:39 +0100

    Added layout component.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,30 +1,13 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
-    <div class="container">
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-
-      
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      …
+    </>
   )
 }
