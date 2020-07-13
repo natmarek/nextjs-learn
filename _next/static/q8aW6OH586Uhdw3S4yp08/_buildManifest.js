43721ec80063b2967b9abbe23188cf75b64d5cc9
ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-07-13 20:33:44 +0000

    Deploying to gh-pages from  @ 7ab6467aaf3256c68b1b0a846bbb430c3b509713 🚀

---
+++ _next/static/q8aW6OH586Uhdw3S4yp08/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/posts/first-post":[a]}}("static\u002Fchunks\u002F99334463bbebe7ded630f6426e3e16e5f3e2611a.e1d5a9e385f72ce68a3d.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
