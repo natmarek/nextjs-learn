255cc7dfbbb6bfdf2b668f0aa33d9cfbfaa4b587
ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-07-13 21:37:24 +0100

    Added css modules.

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
-function Layout({ children }) {
-    return <div>{children}</div>
-  }
-  
-  export default Layout
+import styles from './layout.module.css'
+
+export default function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>
+}
