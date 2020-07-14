8e33a2b8055415b180ce0ceff74bf85d560a8618
ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-07-14 22:16:54 +0100

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
-updateimport Layout, { siteTitle } from '../components/layout'
+import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+
 
 export default function Home() {
   return (
@@ -11,8 +12,7 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Natalia. I'm a Junior Software Developer.</p>
         <p>
-          Before I started coding I have been working as a Behaviour therapist with children and young people with autism{' '}
-          {/* <a href="https://nextjs.org/learn">our Next.js tutorial</a>.) */}
+          Before I started coding I have been working as a Behaviour therapist with children and young people with autism.
         </p>
         <ul>
           <li>JavaScript</li>
